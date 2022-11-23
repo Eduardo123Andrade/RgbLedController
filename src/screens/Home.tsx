d4518064f627a99b2549efd59c769c7c35f2d141
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-11-23 20:38:57 -0300

    Feat - created function to  request permission

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,61 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, View, } from "react-native"
 import { PickerColor, RGBView, Screen } from "../components"
 import { useEffect, useState } from "react"
 import { convertToRGB } from "../utils"
 import { RGBColor } from "../types"
+import { requestMultiplePermissions, requestPermission } from "../utils/requestPermission"
 
+const RESULTS = {
+  DENIED: "denied",
+  GRANTED: "granted",
+  NEVER_ASK_AGAIN: "never_ask_again"
+}
 
 
 export const Home = () => {
   const [currentColor, setCurrentColor] = useState<string>()
   const [rgbColor, setRgbColor] = useState<RGBColor>()
+
+
+  const requestPermission2 = async () => {
+    try {
+      const granted = await requestMultiplePermissions([
+        'ACCESS_FINE_LOCATION',
+        'CAMERA',
+        // 'BLUETOOTH_SCAN',
+        'BLUETOOTH_CONNECT',
+      ])
+      // const granted = await PermissionsAndroid.request(
+      //   PermissionsAndroid.PERMISSIONS.CAMERA,
+      //   {
+      //     title: 'Permissao',
+      //     message: "Preciso da permissao",
+      //     buttonPositive: 'ok',
+      //     buttonNegative: 'cancel'
+      //   }
+      // ).then(res => {
+      //   console.log({ res })
+      //   return res
+      // }).catch(e => console.log({ e }))
+
+
+      // console.log({ granted }, PermissionsAndroid.RESULTS)
+      if (granted) {
+        console.log('allowed')
+      } else {
+        console.log('denied')
+      }
+    } catch (error) {
+      console.log({ error })
+    }
+
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      requestPermission2()
+    }, 2000)
+  }, [])
 
   useEffect(() => {
     if (currentColor) {
