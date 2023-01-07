5d088980589a7adbdd3f0ab6b6abe6d34c2dbbb3
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-07 14:12:01 -0300

    Refactor - Refactored arduino file code

--- arduino/bluetooth.ino
+++ arduino/bluetooth.ino
@@ -8,6 +8,9 @@ int led = 2;
 
 void changeRGBLedColor();
 
+byte switchOn = 'E';
+byte switchOff = 'D';
+
 void setup()
 {
   Serial.begin(9600);
@@ -18,77 +21,36 @@ void setup()
   // bluetoothSerial.write("AT+NAME=Controller\r\n");
   // bluetoothSerial.write("AT+ROLE1\r\n");
   // bluetoothSerial.write("AT+TYPE1");
-  //
-  pinMode(led, OUTPUT);
-  //      digitalWrite(led, HIGH);
 
+  pinMode(led, OUTPUT);
   pinMode(pinButton, INPUT_PULLUP);
 }
 
 void loop()
 {
-
-  // listen for user input and send it to the HC-05
-  //  if (Serial.available()){
-  //    int value =  Serial.read();
-  //
-  //    Serial.println(value);
-  //    bluetoothSerial.write(value);
-  //  }
-
-  // listen for a response from the HC-05 and write it to the serial monitor
   if (bluetoothSerial.available())
   {
     byte receivedByte = bluetoothSerial.read();
     Serial.write(receivedByte);
     Serial.write("\r\n");
 
-    if (receivedByte == 'L')
+    if (receivedByte == switchOn)
     {
       digitalWrite(led, HIGH);
     }
 
-    if (receivedByte == 'D')
+    if (receivedByte == switchOff)
     {
       digitalWrite(led, LOW);
     }
-
-    //    Serial.write("available");
   }
 
   if (Serial.available())
   {
-    //    byte input = Serial.read();
-    //    Serial.write(input);
     bluetoothSerial.write(Serial.read());
-
-    //      bluetoothSerial.write("AT");
-    //    bluetoothSerial.write(Serial.read());
   }
-  //    bluetoothSerial.write(Serial.read());
 
-  // digitalWrite(led, HIGH);
   changeRGBLedColor();
-
-  //  if (bluetoothSerial.available())
-  //  {
-  //    byte receivedByte = bluetoothSerial.read();
-  //
-  //    if (receivedByte == 'L')
-  //    {
-  //      digitalWrite(led, HIGH);
-  //    }
-  //
-  //    if (receivedByte == 'D')
-  //    {
-  //      digitalWrite(led, LOW);
-  //    }
-
-  // boolean acao = bitRead(receivedByte, 5); // Lê se o comando é para ligar ou desligar
-  // byte porta = bitClear(bitClear(receivedByte, 6), 5);
-
-  // digitalWrite(pinPortas[porta - 1], !acao);
-  //  }
 }
 
 void changeRGBLedColor()
@@ -107,21 +69,17 @@ void changeRGBLedColor()
     {
       Serial.write("Desligado\r\n");
       digitalWrite(led, LOW);
-      bluetoothSerial.write("D");
+      bluetoothSerial.write(switchOff);
     }
     else
     {
       Serial.write("Ligado\r\n");
       digitalWrite(led, HIGH);
-      bluetoothSerial.write("L");
+      bluetoothSerial.write(switchOn);
     }
     //   // redColor = randomNumber();
     //   // greenColor = randomNumber();
     //   // blueColor = randomNumber();
-
-    //   // RGBColor(redColor, greenColor, blueColor);
-
-    //  isRetention = !isRetention;
   }
   previousButtonValue = buttonValue;
   delay(100);
